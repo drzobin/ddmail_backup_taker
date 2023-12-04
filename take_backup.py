0c932de8fe168787026cde92ea1d106c5c4a9b41
drobin <robin.larsson@protonmail.ch>
2023-12-04 15:35:14 +0100

    Parse config file

--- take_backup.py
+++ take_backup.py
@@ -1,2 +1,16 @@
 import configparser
 
+# Import configuration file.
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Working folder.
+tmp_folder = config["DEFAULT"]["tmp_folder"]
+
+# Backups will be saved to this folder.
+save_backups_to = config["DEFAULT"]["save_backups_to"]
+
+# The folder to take backups on.
+folders_to_backup = str.split(config["DEFAULT"]["folders_to_backup"])
+
+print(folders_to_backup)
