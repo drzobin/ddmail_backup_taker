fbbbf4984de9445849d4a02052bbab1da41e8973
drobin <robin.larsson@protonmail.ch>
2023-12-04 15:38:37 +0100

    Create folers

--- take_backup.py
+++ take_backup.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 # Import configuration file.
 config = configparser.ConfigParser()
@@ -13,4 +14,10 @@ save_backups_to = config["DEFAULT"]["save_backups_to"]
 # The folder to take backups on.
 folders_to_backup = str.split(config["DEFAULT"]["folders_to_backup"])
 
-print(folders_to_backup)
+# Create working folder.
+if not os.path.exists(tmp_folder): 
+    os.makedirs(tmp_folder) 
+
+# Create folder to save backups to
+if not os.path.exists(save_backups_to): 
+    os.makedirs(save_backups_to) 
