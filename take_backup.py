8b24d05c3cdfd49521310b03b4b7b3e207645cf3
drz <me@drz.se>
2023-12-09 08:18:15 +0100

    Adding logging

--- take_backup.py
+++ take_backup.py
@@ -2,7 +2,8 @@ import configparser
 import os
 import subprocess
 import shutil
-from datetime import date
+import logging
+import datetime
 
 # Import configuration file.
 config = configparser.ConfigParser()
@@ -26,6 +27,9 @@ mariadbdump_bin = config["DEFAULT"]["mariadbdump_bin"]
 # Mariadb root password.
 mariadb_root_password = config["mariadb"]["root_password"]
 
+# Configure logging.
+logging.basicConfig(filename=config["logging"]["logfile"], format='%(asctime)s: %(levelname)s: %(message)s', level=logging.ERROR)
+
 # Create working folder.
 if not os.path.exists(tmp_folder): 
     os.makedirs(tmp_folder) 
@@ -35,7 +39,7 @@ if not os.path.exists(save_backups_to):
     os.makedirs(save_backups_to)
 
 # Create working folder for todays date.
-today = str(date.today())
+today = str(datetime.date.today())
 if not os.path.exists(tmp_folder + "/" + today): 
     os.makedirs(tmp_folder + "/" + today)
 tmp_folder_date = tmp_folder + "/" + today
@@ -47,11 +51,11 @@ for folder in folders_to_backup:
     try:
         output = subprocess.run([tar_bin,"-czf",tmp_folder_date + "/" + backup_name,folder], check=True)
         if output.returncode != 0:
-            print("error: returncode of cmd tar is non zero")
+            logging.error("returncode of cmd tar is non zero")
     except subprocess.CalledProcessError as e:
-        print("error: returncode of cmd tar is non zero")
+        logging.error("returncode of cmd tar is non zero")
     except:
-        print("error: unkonwn exception running subprocess with tar")
+        logging.error("unkonwn exception running subprocess with tar")
 
 # Take backup of mariadb databases.
 if config["mariadb"].getboolean("take_backup") == True:
@@ -59,11 +63,11 @@ if config["mariadb"].getboolean("take_backup") == True:
         f = open(tmp_folder_date + "/" + "full_db_dump.sql","w")
         output = subprocess.run([mariadbdump_bin,"-h","localhost","--all-databases","-uroot","-p" + mariadb_root_password], check=True, stdout = f)
         if output.returncode != 0:
-            print("error: returncode of cmd mysqldump is non zero")
+            logging.error("returncode of cmd mysqldump is non zero")
     except subprocess.CalledProcessError as e:
-        print("error: returncode of cmd mysqldump is non zero")
+        logging.error("returncode of cmd mysqldump is non zero")
     except:
-        print("error: unknown exception running subprocess with mysqldump")
+        logging.error("unknown exception running subprocess with mysqldump")
 
 # Compress all files with zip.
 shutil.make_archive(save_backups_to + "/" + "backup." + today, 'zip', tmp_folder_date)
