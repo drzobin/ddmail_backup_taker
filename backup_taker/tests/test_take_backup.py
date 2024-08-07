c2eded8d3477bd999d8bae0a82948342e9d218c0
drz <me@drz.se>
2024-08-07 17:55:40 +0200

    Add test for backup_folders()

--- backup_taker/tests/test_take_backup.py
+++ backup_taker/tests/test_take_backup.py
@@ -1,4 +1,6 @@
-from backup_taker.take_backup import sha256_of_file
+from backup_taker.take_backup import sha256_of_file, backup_folders
+import os
+import shutil
 
 # Testfile used in many testcases.
 TESTFILE_PATH = "tests/test_file.txt"
@@ -8,16 +10,41 @@ f = open(TESTFILE_PATH, "r")
 TESTFILE_DATA = f.read()
 
 
-# Test that sha256 checksum is correct.
 def test_sha256_of_file_test1():
+    """Test that sha256 checksum is correct."""
     sha256 = sha256_of_file(TESTFILE_PATH)
 
     assert sha256 == SHA256
 
 
-# Test to open a file that do not exist.
 def test_sha256_of_file_test2():
+    """Test to open a file that do not exist."""
     file_path = "/tmp/nofile"
     sha256 = sha256_of_file(file_path)
 
     assert sha256 is None
+
+
+def test_backup_folders_test1():
+    """Test to tar two folders."""
+    test_folders = ["/tmp/test1", "/tmp/test2"]
+    for folder in test_folders:
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    dst_test_folder = "/tmp/test3"
+    if os.path.exists(dst_test_folder):
+        shutil.rmtree(dst_test_folder)
+    if not os.path.exists(dst_test_folder):
+        os.makedirs(dst_test_folder)
+
+    tar_bin = "/usr/bin/tar"
+    folders_to_backup = test_folders
+    dst_folder = dst_test_folder
+
+    backup_folders(tar_bin, folders_to_backup, dst_folder)
+
+    assert os.path.exists(dst_test_folder + "/" + "_tmp_test1.tar.gz") is True
+    assert os.path.exists(dst_test_folder + "/" + "_tmp_test2.tar.gz") is True
