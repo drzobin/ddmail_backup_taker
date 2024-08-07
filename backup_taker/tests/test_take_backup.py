bfee018bd7bfe34c413941c781860b9a989a155c
drz <me@drz.se>
2024-08-07 14:53:20 +0200

    Fix spaces and newlines

--- backup_taker/tests/test_take_backup.py
+++ backup_taker/tests/test_take_backup.py
@@ -7,11 +7,13 @@ SHA256 = "4677942dfa3e74b5dea7484661a2485bb73ba422eb72d311fdb39372c019c615"
 f = open(TESTFILE_PATH, "r")
 TESTFILE_DATA = f.read()
 
-# Test that sha256 checksum is correct. 
+
+# Test that sha256 checksum is correct.
 def test_sha256_of_file_test1():
     sha256 = sha256_of_file(TESTFILE_PATH)
 
     assert sha256 == SHA256
+
 
 # Test to open a file that do not exist.
 def test_sha256_of_file_test2():
